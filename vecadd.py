691e5e038103ab2678ce92d081b773cbf6379bf0
manas <2023ucp1843@mnit.ac.in>
2025-11-13 15:07:49 +0530

    added vecadd.py file

---
+++ vecadd.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = list(map(float, input("Enter the first vector (space-separated): ").split()))
+b = list(map(float, input("Enter the second vector (space-separated): ").split()))
+
+if len(a) != len(b):
+    print("Error: Vectors must be of the same length.")
+else:
+    dot_product = sum(x * y for x, y in zip(a, b))
+    print("Dot product:", dot_product)
+print("*output using old code*")
+
+print(np.dot(a, b))
